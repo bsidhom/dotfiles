f4ea11343fd8f78eb564603577589f1c0367041b
Ben Sidhom <bsidhom@gmail.com>
2014-05-28 13:53:02 -0700

    Add Xresources configuration.
    Added Xresources configuration (for urxvt theming) and updated dotfiles install
    script to handle files that must be preprocessed by cpp (as many distributions
    will not pass ~/.Xresources through a preprocessor at login time). Pull in
    changes from zsh.

--- replace_dotfiles.sh
+++ replace_dotfiles.sh
@@ -7,7 +7,7 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "$HOME" >/dev/null
 while read -r CONFIG_FILE ; do
     if [ -d "$CONFIG_FILE" ] ; then
-        "skipping directory: $CONFIG_FILE"
+        echo "skipping directory: $CONFIG_FILE"
     else
         if [ -f "$CONFIG_FILE" ] ; then
             echo "overwriting configuration file: $CONFIG_FILE"
@@ -17,7 +17,7 @@ while read -r CONFIG_FILE ; do
 done <"$DOTFILES_DIR/static_files.txt"
 while read -r CONFIG_FILE ; do
     if [ -d "$CONFIG_FILE" ] ; then
-        "skipping directory: $CONFIG_FILE"
+        echo "skipping directory: $CONFIG_FILE"
     else
         if [ -f "$CONFIG_FILE" ] ; then
             echo "overwriting configuration file: $CONFIG_FILE"
@@ -25,3 +25,14 @@ while read -r CONFIG_FILE ; do
         envsubst <"${DOTFILES_DIR}/${CONFIG_FILE}" >"./${CONFIG_FILE}"
     fi
 done <"$DOTFILES_DIR/dynamic_files.txt"
+while read -r CONFIG_FILE ; do
+    if [ -a "$CONFIG_FILE" ] ; then
+        if [ -f "$CONFIG_FILE" ] ; then
+            echo "overwriting configuration file: $CONFIG_FILE"
+        else
+            echo "skipping non-regular file: $CONFIG_FILE"
+        fi
+    else
+        cpp "${DOTFILES_DIR}/${CONFIG_FILE}.src" "./${CONFIG_FILE}"
+    fi
+done <"$DOTFILES_DIR/preprocessed_files.txt"
